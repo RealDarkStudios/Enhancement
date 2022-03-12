a61410c74d09798537388f2d71a3f101a2acaf31
realDarkGamerYT <jmorton@digitalunderworlds.com>
2022-03-12 13:48:22 -0600

    (13:48 CST, Sat 03/12/22) <11 Changes>: Add Dragonite Block and Ingot and their respective recipes and loot table.

--- src/main/java/net/darkstudios/enhancement/block/ModBlocks.java
+++ src/main/java/net/darkstudios/enhancement/block/ModBlocks.java
@@ -2,15 +2,23 @@ package net.darkstudios.enhancement.block;
 
 import net.darkstudios.enhancement.EnhancementMod;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.MapColor;
+import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModBlocks {
 
     //Mod Blocks Here (Blocks will be sorted into categories as more get added.)
+    public static final Block DRAGONITE_BLOCK = registerBlock("dragonite_block", new Block(FabricBlockSettings.of(Material.METAL).strength(70F, 1400F).mapColor(MapColor.TERRACOTTA_BLACK).sounds(BlockSoundGroup.NETHERITE).requiresTool()), new FabricItemSettings().group(ItemGroup.MISC).fireproof().rarity(Rarity.RARE));
 
     //Register Block to the Minecraft Registry, so that it exists!
     private static Block registerBlock(String name, Block block, FabricItemSettings settings) {
